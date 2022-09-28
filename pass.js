fe7177b058636eec6e615794eb2f732d77e266f7
souravrn01 <112268288+souravrn01@users.noreply.github.com>
2022-09-28 15:33:48 +0530

    validations updation

--- pass.js
+++ pass.js
@@ -1,30 +1,27 @@
-var pass= document.getElementsById('pword')
+var pass= document.getElementsByName('password')
 var msg = document.getElementById('message')
 var str = document.getElementById('strength')
 var reg = /^[\d\w@-]{8,20}$/i
 
-if(reg.test(pass.value)){
-    pass.addEventListener('input',()=>{
 
-        if(pass.value.length < 4){
+    pass.addEventListener('keyup',(e)=>{
+        if(reg.test(pass.value)){
+
+        if(e.target.value.length < 4){
             str.innerHTML= "weak"
             msg.style.color = "#ff5925"
         }
-        else if(pass.value.length >= 4 && pass.value.length <8){
+        else if(e.target.value.length  >= 4 && e.target.value.length  <8){
             str.innerHTML= "medium"
             msg.style.color = "orange"
         }
 
-        else if(pass.value.length >= 8){
+        else if(e.target.value.length  >= 8){
             str.innerHTML = "strong"
             msg.style.color = "#26d730"
         }
-
+    }
     })
-}
-else{
-    str.innerHTML = "invalid"
-    msg.style.color = "#ff5925"
 
-}
+
 
--- val.js
+++ val.js
@@ -1,4 +1,10 @@
 const inputs = document.querySelectorAll("input")
+var pword=document.querySelector( "#pword")
+var msg = document.querySelector('#message')
+var str = document.querySelector('#strength')
+var valid = document.querySelector('#validity')
+
+
 
 //regex patterns
 
@@ -6,7 +12,7 @@ const patterns = {
     number: /^((\d){3}-?(\d){3}-?(\d){4})$|^((\d){3}\.?(\d){3}\.?(\d){4})$|^((\d){3}\s?(\d){3}\s?(\d){4})$/,
     username: /^[a-z\d]{5,12}$/i,
     email:/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
-    password:/^[\d\w@-]{8,20}$/i
+    password:/^[\d\w@]{8,20}$/i
 };
 
 // validation functions
@@ -28,4 +34,42 @@ inputs.forEach((input) => {
 });
 
 // password validation
+
+
+passpattern = patterns.password
+function passtr(data,val){
+    if(data <8){
+        str.innerText= "very weak"
+        msg.style.color = "#ff5925"
+        valid.innerText = " and invalid. add numbers, @, capital letters "
+
+    }
+    else if(data >=8 && data <13 && val=="valid"){
+        str.innerText= " okay"
+        msg.style.color = "orange"
+        valid.innerText = " and valid"
+    }
+    else if(data >= 13 && data <=20 && val=="valid"){
+        str.innerText = "strong"
+        msg.style.color = "#26d730" 
+        valid.innerText = " and valid"  
+    }
+    else{
+        str.innerText = "too long, max: 20 char"
+        msg.style.color = "#ff5925"
+        valid.innerText = ""
+    }
+
+}
+
+pword.addEventListener('keyup',(el)=>{
+    passtr(el.target.value.length,el.target.className)
+
+
+})
+
+// submition validation
+
+
+
 
